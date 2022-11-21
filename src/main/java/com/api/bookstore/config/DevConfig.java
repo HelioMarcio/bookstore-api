672a349cc4fb9f10e57982f47ddcc06a95a3120d
HelioMarcio <heliomarcio100@gmail.com>
2022-11-21 10:46:05 -0300

    criacao do perfil de dev

--- src/main/java/com/api/bookstore/config/DevConfig.java
+++ src/main/java/com/api/bookstore/config/DevConfig.java
@@ -1,20 +1,22 @@
 package com.api.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.api.bookstore.service.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 	
-	@Bean
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+    @Bean
 	public void instanciaBaseDeDados() {
 		this.dbService.instanciaBaseDeDados();
 	}
