0116c641ff57817d4ae0e5362d8a90daf1bfb83b
HelioMarcio <heliomarcio100@gmail.com>
2022-12-03 23:03:44 -0300

    UPDATE para Categoria

--- src/main/java/com/api/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/api/bookstore/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,12 @@ public class CategoriaResource {
 	public ResponseEntity<CategoriaDTO> create(@RequestBody CategoriaDTO obj) {
 		obj = service.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);
+		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto) {
+		objDto = service.update(id, objDto);
+		return ResponseEntity.ok().body(objDto);
 	}
 }
--- src/main/java/com/api/bookstore/service/CategoriaService.java
+++ src/main/java/com/api/bookstore/service/CategoriaService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.api.bookstore.domain.Categoria;
 import com.api.bookstore.dtos.CategoriaDTO;
@@ -18,6 +21,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 
+	@Transactional(readOnly = true)
 	public CategoriaDTO findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		Categoria entity = obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -25,16 +29,31 @@ public class CategoriaService {
 		return new CategoriaDTO(entity);
 	}
 
+	@Transactional(readOnly = true)
 	public List<CategoriaDTO> findAll() {
 		List<Categoria> list = repository.findAll();
 		return list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 	}
 
+	@Transactional
 	public CategoriaDTO create(CategoriaDTO dto) {
 		Categoria obj = new Categoria();
 		obj.setNome(dto.getNome());
 		obj.setDescricao(dto.getDescricao());
 		obj = repository.save(obj);
 		return new CategoriaDTO(obj);
+	}
+
+	@Transactional
+	public CategoriaDTO update(Integer id, CategoriaDTO objDto) {
+		try {
+			Categoria obj = repository.getReferenceById(id);
+			obj.setNome(objDto.getNome());
+			obj.setDescricao(objDto.getDescricao());
+			obj = repository.save(obj);
+			return new CategoriaDTO(obj);
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Objeto não encontrado: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 	}
 }
